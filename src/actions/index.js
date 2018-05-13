4ec0fcf065387310f36cb738c43031d2d701d7d5
unknown <matejcsok@hotmail.com>
2018-05-13 18:40:38 +0200

    initial commit for reminder_pro

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+import {ADD_REMINDER} from '../constants';
+import {DELETE_REMINDER} from '../constants';
+import {CLEAR_REMINDERS} from '../constants';
+
+export const addReminder = (text, dueDate) => {
+    const action = {
+        type: ADD_REMINDER,
+        text,
+        dueDate,
+    };
+    console.log('action in addReminder', action);
+    return action
+};
+
+export const deleteReminder = (id) => {
+    const action =  {
+        type: DELETE_REMINDER,
+        id
+    };
+    console.log('deleting in actions', action);
+    return action;
+};
+
+export const clearReminders = () => {
+    return {
+        type: CLEAR_REMINDERS,
+    };
+};
